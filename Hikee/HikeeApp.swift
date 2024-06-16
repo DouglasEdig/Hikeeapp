35c5cf7e7f5ec7c54adecbf7cdf6e43589af3cee
Douglas Silva <douglas.edig@outlook.com>
2024-06-16 17:59:47 -0300

    Initial Commit

---
+++ Hikee/HikeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeeApp.swift
+//  Hikee
+//
+//  Created by Douglas Matheus da Silva on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
