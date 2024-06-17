18c5c36c53de14cb10d0b20f515c35a0930ccc19
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-17 12:52:57 -0300

    Separating image composition from the main view

--- Hikee/Component/CardView.swift
+++ Hikee/Component/CardView.swift
@@ -8,65 +8,100 @@
 import SwiftUI
 
 struct CardView: View {
+    // Layout properties
+    var cardWidth: CGFloat = 320
+    var cardHeight: CGFloat = 570
+    
+    // Main View
     var body: some View {
         
         ZStack {
             CustomBackgroundView()
-            
-            VStack {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Hiking")
-                            .fontWeight(.black)
-                            .font(.system(size: 52))
-                            .foregroundStyle(
-                                LinearGradient(
-                                    colors: [
-                                        .customGrayLight,
-                                        .customGrayMedium],
-                                    startPoint: .top,
-                                    endPoint: .bottom)
-                            )
-                        
-                        Spacer()
-                        
-                        Button {
-                            print("the button was pressed")
-                        }label: {
-                            CustomButtonView()
-                        }
-                        
+            cardContent
+        }
+        .frame(
+            width: cardWidth,
+            height: cardHeight
+        )
+    }
+}
+
+// MARK: - Card Content
+extension CardView {
+    var cardContent: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Hiking")
+                        .fontWeight(.black)
+                        .font(.system(size: 52))
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGrayLight,
+                                    .customGrayMedium],
+                                startPoint: .top,
+                                endPoint: .bottom)
+                        )
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("the button was pressed")
+                    }label: {
+                        CustomButtonView()
                     }
                     
-                    Text("Fun and enjoyable outdoor activity for friends and families.")
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                        .italic()
-                        .foregroundColor(.customGrayMedium)
-                }
-                .padding(.horizontal,30)
-                
-                
-                ZStack {
-                    Circle()
-                        .fill(
-                            LinearGradient(
-                                colors:[
-                                    Color("ColorIndigoMedium"),
-                                    Color("ColorSalmonLight")
-                                ],
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .frame(width: 256, height: 256)
-                    Image("image-1")
-                        .resizable()
-                        .scaledToFit()
                 }
                 
+                Text("Fun and enjoyable outdoor activity for friends and families.")
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    .italic()
+                    .foregroundColor(.customGrayMedium)
             }
+            .padding(.horizontal,30)
+            
+            imageComposition
         }
-        .frame(width: 320, height: 570)
+    }
+}
+
+// MARK: - Image Composition
+extension CardView {
+    // Layout properties
+    var imageName: String { "image-1" }
+    var circleSize: CGFloat { 256 }
+    var backgroundColors: [Color] {
+        [
+            Color("ColorIndigoMedium"),
+            Color("ColorSalmonLight")
+        ]
+    }
+    
+    // Views
+    var imageComposition: some View {
+        ZStack {
+            backgroundCircle
+            mainImage
+        }
+    }
+    
+    var backgroundCircle: some View {
+        Circle()
+            .fill(
+                LinearGradient(
+                    colors: backgroundColors,
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+            )
+            .frame(width: circleSize, height: circleSize)
+    }
+    
+    var mainImage: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFit()
     }
 }
 
