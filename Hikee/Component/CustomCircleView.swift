fcb69b79dee2084a80f8aeea6097c65917dfe941
Douglas Silva <douglas.edig@outlook.com>
2024-06-19 22:53:39 -0300

    feat: add motion in project

---
+++ Hikee/Component/CustomCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCircleView.swift
+//  Hikee
+//
+//  Created by Douglas Matheus da Silva on 19/06/24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
---
+++ Hikee/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Hikee
+//
+//  Created by Douglas Matheus da Silva on 19/06/24.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
