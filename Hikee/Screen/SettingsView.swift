be013da5541e28bbff8c13d08cf5442b41ce8f11
Douglas Silva <douglas.edig@outlook.com>
2024-06-22 23:48:55 -0300

    feat: settings icon finish and finifh app

--- Hikee/Screen/SettingsView.swift
+++ Hikee/Screen/SettingsView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 
 struct SettingsView: View {
+  //MARK:- PROPERTIES
+  
+  private let alternateAppIcons: [String] = [
+    "AppIcon-MagnifyingGlass",
+    "AppIcon-Map",
+    "AppIcon-Mushroom",
+    "AppIcon-Camera",
+    "AppIcon-Backpack",
+    "AppIcon-Campfire",
+  ]
+  
+  
   var body: some View {
     List {
       //MARK: - SECTION: HEADER
@@ -65,6 +77,48 @@ struct SettingsView: View {
       
       //MARK: - SECTION: ICONS
       
+      Section(header: Text("Alternate Icons")) {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(spacing: 12) {
+            ForEach(alternateAppIcons.indices, id: \.self) {
+              item in
+              Button {
+                print("icon \(alternateAppIcons[item]) was pressed")
+                
+                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) {
+                  error in
+                  if error != nil {
+                    print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                  } else {
+                    print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                    
+                  }
+                }
+                
+                
+              } label: {
+                Image("\(alternateAppIcons[item])-Preview")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 80, height: 80)
+                  .cornerRadius(16)
+              }
+            }
+            .buttonStyle(.borderless)
+          }
+        }
+        .padding(.top, 12)
+        
+        Text("Choose your favourite app icon from the collection above.")
+          .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.secondary)
+          .font(.footnote)
+          .padding(.bottom, 12)
+      } //SECTION ICONS
+      .listRowSeparator(.hidden)
+      
       //MARK: - SECTION: ABOUT
       
       Section (
@@ -90,7 +144,7 @@ struct SettingsView: View {
         
         CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "My Portfolio", rowLinkDestination: "https://douglassilva.vercel.app")
         
-      }//:SECTION
+      }//:SECTION - ABOUT
     }
   }
 }
