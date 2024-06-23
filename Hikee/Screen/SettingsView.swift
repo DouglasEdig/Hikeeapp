949113409bdbe3898d18e3f9b201d40553d95974
Douglas Silva <douglas.edig@outlook.com>
2024-06-22 22:49:34 -0300

    feat: finish about content

--- Hikee/Screen/SettingsView.swift
+++ Hikee/Screen/SettingsView.swift
@@ -67,6 +67,30 @@ struct SettingsView: View {
       
       //MARK: - SECTION: ABOUT
       
+      Section (
+        header: Text("ABOUT THE APP"),
+        footer: HStack {
+          Spacer()
+          Text("Copyright @ All right reserved.")
+          Spacer()
+        }
+          .padding(.vertical, 8)
+      ) {
+        CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "Hike", rowTintColor: .blue)
+        
+        CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "IOS", rowTintColor: .red)
+        
+        CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+        
+        CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+        
+        CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Doug", rowTintColor: .mint)
+        
+        CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+        
+        CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "My Portfolio", rowLinkDestination: "https://douglassilva.vercel.app")
+        
+      }//:SECTION
     }
   }
 }
